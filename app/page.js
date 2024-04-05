95d9f83c61b5d2e247f8be416029eac8155ddb8c
GraceJ0105 <gejohnson_15@hotmail.com>
2024-04-05 14:55:01 +0100

    testing git updates

--- app/page.js
+++ app/page.js
@@ -27,6 +27,7 @@ const [message, setMessage] = useState("")
       <h1>
         Hello world
       </h1>
+      <h2>Testing</h2>
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleClick}>
   Click Me
 </button>
